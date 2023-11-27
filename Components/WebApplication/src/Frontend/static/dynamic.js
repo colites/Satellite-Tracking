9a4e479c495c473024c984e8d5e5238036b03d90
colites <ezequielgomez2022@u.northwestern.edu>
2023-11-27 08:12:30 -0600

    Add files via upload

--- Components/WebApplication/src/Frontend/static/dynamic.js
+++ Components/WebApplication/src/Frontend/static/dynamic.js
@@ -9,7 +9,7 @@ async function SendCoordinates(latitude, longitude){
         longitude: longitude
   };
 
-    const response = await fetch(`https://0.0.0.0:5001/send-coordinates`, {
+    const response = await fetch(`https://backend-q6r6.onrender.com/send-coordinates`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
